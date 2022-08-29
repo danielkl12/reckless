b6bcdb3f233b8a2deed18d6223dc4103df6f3e95
Daniel Klauer <danielkl12@icloud.com>
2022-08-29 13:04:15 -0500

    navbar update

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -20,12 +20,13 @@ export default class Navbar extends Component {
                     <nav id="nav-wrap" >
                         
                         <ul id="nav" className="nav" style= {{listStyle: 'none', }}>
-                            <li className="current"><a href="#home">Home</a></li>
+                            
                             <li><a href="#about">About</a></li>
                             <li><a href="#footer">Contact</a></li>
                             <li><a href="#photos">Photos</a></li>
-                            
+                            <li><a href="#music">Music</a></li>
                             <li><a href="#video">Video</a></li>
+                            {/* <li><a href="#music"></a>Music</li> */}
                             <li><a href={weddingpdf}>Wedding Details</a></li>
                             
                             
