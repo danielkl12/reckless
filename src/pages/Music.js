f709f80a904e8b7f6c14c14cac0c53e6e0c25704
Daniel Klauer <danielkl12@icloud.com>
2022-12-01 18:22:08 -0600

    sizing attempt soundcloud player

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -2,10 +2,35 @@
 import ReactPlayer from "react-player"
 import { Component } from 'react';
 export default class Music extends Component {
+    
+    constructor(props) {
+        super(props)
+        this.state = {
+        isDesktop: false
+    };
+    this.updatePredicate = this.updatePredicate.bind(this);
+    }
+
+    componentDidMount = () => {
+            this.updatePredicate();
+    window.addEventListener("resize", this.updatePredicate);
+        }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updatePredicate);
+        }
+    
+    updatePredicate() {
+        this.setState({ isDesktop: window.innerWidth > 600,});
+        }
+
     render() {
         let portfolioData = this.props.portfolioData;
+        const isDesktop = this.state.isDesktop;
+        
         return(
             <section id="music">
+                {isDesktop ? (
                 <div className="row">
                     <div className="columns">
                     <div className="six columns main-col">
@@ -42,14 +67,54 @@ export default class Music extends Component {
                             </div>
                         </div>
                     </div>
+                ) : (
+                    
+                    <div className="row">
+                    <div className="columns">
+                    <div className="six columns main-col">
+                        <h2 style={{color: "white", fontSize:"50px", fontFamily:"squealer"}}>Music</h2>
+                        <div style={{color:"white", marginLeft:"50px", marginRight:"50px"}}> 
+                            
+                        </div>
+
+                        
+                    </div>
+                    </div>
+
+
+                    
+                <ReactPlayer style={{opacity:"20px", width:"10px"}}
+        url="https://soundcloud.com/user-561841802"
+                                                    />
+                       
+
+                        <p>
+                            {
+                                portfolioData.aboutMe
+                            }
+
+                        </p>
+
+                        <div className="row">
+                            <div className="columns contact-details">
+                                
+                                    
+                                    
+                                    
+                              
+                            </div>
+                        </div>
+                    </div>
                 
+                
+                )}
             </section>
             
 
 
 
  
-    
+                        
   )
         }  }
 
