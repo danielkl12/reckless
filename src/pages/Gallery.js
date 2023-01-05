09eee2ea1c83df499e1fcfadea7e10c82acd6c46
Daniel Klauer <danielkl12@icloud.com>
2023-01-05 17:57:25 -0600

    updated photo section

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -7,12 +7,130 @@ import joe from '../srcimages/joe.jpg'
 import mikeg from '../srcimages/mikeg.jpg'
 import crowd from '../srcimages/crowd.jpg'
 
-import ReactDOM, { render } from 'react-dom'
+export default class Photos extends Component {
 
 
-// import { Gallery } from 'react-grid-gallery';
-// import Imgix from "react-imgix";
-// import './style.css';
+
+    //renders about page with profile picture, about me sections, etc
+    render() {
+
+        return (
+            <section id="photos">
+                <div className="row">
+                    <div className="columns">
+
+
+                    </div>
+
+
+                    <div className="six columns main-col">
+                        <h2 style={{color: "white", fontSize:"50px", fontFamily:"squealer"}}>Photos</h2>
+                        <div style={{color:"white", marginLeft:"50px", marginRight:"50px"}}> 
+
+                        </div>
+
+        <div>  
+                <section id="card" class="cards">
+                        <article className="card">
+
+                            <img src={dan} alt="dan" id='dan' style={{marginRight:"30px"}}/>
+                        </article>
+
+
+                        <article className="card">
+                            <img src={mike} alt="mike" id='mike'/>
+                        </article>
+
+                        <article className="card">
+                            <img src={danny} alt="danny" id='danny' style={{marginLeft:"30px"}}/>
+                        </article>
+
+                        <article className="card">
+                            <img src={joe} alt="joe" id='joe' style={{marginLeft:"30px"}}/>
+                        </article>
+
+                        <article className="card">
+                            <img src={mikeg} alt="mikeg" id='mikeg' style={{marginLeft:"30px"}}/>
+                        </article>
+                        <article className="card">
+                            <img src={crowd} alt="crowds" id='crowd' style={{marginLeft:"30px"}}/>
+                        </article>
+
+
+
+                </section>
+        </div>  
+
+
+
+
+
+
+
+                        <div className="row">
+                            <div className="columns contact-details">
+
+
+
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        );
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+// import { Gallery } from "react-grid-gallery";
+
+// const images = [
+//    {
+//       src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
+//       width: 320,
+//       height: 174,
+//       isSelected: true,
+//       caption: "After Rain (Jeshu John - designerspics.com)",
+//    },
+//    {
+//       src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+//       width: 320,
+//       height: 212,
+//       tags: [
+//          { value: "Ocean", title: "Ocean" },
+//          { value: "People", title: "People" },
+//       ],
+//       alt: "Boats (Jeshu John - designerspics.com)",
+//    },
+
+//    {
+//       src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
+//       width: 320,
+//       height: 212,
+//    },
+// ];
+
+// <Gallery images={images} />
+
+
+// export default Gallery;
+
+
+
+
+
 
 // const images = [
 //   "RAT+3_22+Crem+de+la+crem-047.jpg",
@@ -43,75 +161,3 @@ import ReactDOM, { render } from 'react-dom'
 // );
 
 // export default Gallery;
-
-class Gallery extends React.Component {
-    
-  constructor(props) {
-      super(props);
-      this.state = { 
-          images: [
-              {src:'//placeimg.com/600/400?text=1'},
-              {src:'//placeimg.com/600/400?text=2'},
-              {src:'//placeimg.com/600/400/any'},
-              {src:'//placeimg.com/600/400?text=4'},
-              {src:'//placeimg.com/600/400?text=5'},
-              {src:'//placeimg.com/600/400?text=6'},
-          ],
-          currentSelection: {},
-      };
-      this.handleClick = this.handleClick.bind(this);
-  }
-  
-  componentDidMount() {
-      // set first image selected
-      this.setState({ currentSelection: this.state.images[0] });
-  }
-  
-  handleClick(val) {
-      //console.log(val)
-      this.setState({ currentSelection: val });
-  }
-  
-  render(){
-      var { images, currentSelection } = this.state;
-      return(
-      <div>
-          <div className="photo-grid">
-              <div className="col-md">
-                  <div className="no-gutters">
-                  {images.map((val, k) => {
-                      return (
-                      <div className="col-sm-4" key={k}>
-                          <img src={val.src} className={'img-fluid ' + (val.src===currentSelection.src?'p-1':'')} onClick={() => this.handleClick(val)} />
-                      </div>)
-                      })
-                  }
-                   </div>
-              </div>
-              <div className="col-md">
-                  <h3 className="font-weight-light">{ currentSelection.name }</h3>
-                  <p>{ currentSelection.desc }</p>
-              </div>
-          </div>
-      </div>
-      )
-  }
-};
-
-export default Gallery;
-// class App extends React.Component {
-// render() {
-//   return (
-//   <div className="container-fluid py-3">
-//       <h4 className="text-center font-weight-light text-light mb-3">React Gallery with Bootstrap</h4>
-//       <ImageGrid />
-//   </div>
-//   )
-// }
-// }
-
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root')
-
-// )
