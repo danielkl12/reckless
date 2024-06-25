0653f3dde13f59e6847df47d2e9d1817d427517c
Daniel Klauer <danielkl12@icloud.com>
2024-06-24 19:22:09 -0500

    update app.js for react helmet seo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-
 import React, { useState, useEffect } from 'react';
+import { Helmet } from 'react-helmet';
 import 'animate.css';
 import './App.css';
 import 'aos/dist/aos.css';
@@ -13,7 +13,6 @@ import Video from './pages/Video';
 import Footer from './pages/Footer';
 import Contact from './pages/Contact';
 
-
 const App = (props) => {
     const [isDesktop, setIsDesktop] = useState(window.innerWidth > 900);
 
@@ -22,54 +21,46 @@ const App = (props) => {
     };
 
     useEffect(() => {
-        Aos.init({duration: 650,
-          once: true});
-        
-
+        Aos.init({ duration: 650, once: true });
         window.addEventListener('resize', updatePredicate);
         return () => {
             window.removeEventListener('resize', updatePredicate);
         };
     }, []);
 
-    
-  
-  
-  return (
-    <div>
-    { isDesktop ? (
-    <div className="App">
-      <Navbar/>
-      <About/>
-      <Contact/>
-      <Testimonials/>
-      <Photos/>
-      <Music/>
-      <Video/>
-      <Footer/>
-      </div>
-    ) : (
-      <div className="mobile">
-      <Navbar/>
-      <About/>
-      <Contact/>
-      <Testimonials/>
-      <Music/>
-      {/* <Photos/> */}
-      <Video/>
-      <Footer/>
-
-      </div>
-    )}
-    </div>
-    
-    
-  );
-  }
-
+    return (
+        <div>
+            <Helmet>
+                <title>Your Site Title</title>
+                <link rel="canonical" href="https://www.example.com" />
+                <meta name="description" content="Your site description" />
+                {/* Add other meta tags as needed */}
+            </Helmet>
+            {isDesktop ? (
+                <div className="App">
+                    <Navbar />
+                    <About />
+                    <Contact />
+                    <Testimonials />
+                    <Photos />
+                    <Music />
+                    <Video />
+                    <Footer />
+                </div>
+            ) : (
+                <div className="mobile">
+                    <Navbar />
+                    <About />
+                    <Contact />
+                    <Testimonials />
+                    <Music />
+                    {/* <Photos /> */}
+                    <Video />
+                    <Footer />
+                </div>
+            )}
+        </div>
+    );
+}
 
 export default App;
-
-
-
-                
